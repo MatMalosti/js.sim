1fae45fd1e01e74e2060ac063a35d26c70fb2ba4
MatMalosti <0000110185439xsp@al.educacao.sp.gov.br>
2024-08-04 23:16:31 -0300

    adicionando arquivos .js e .css

---
+++ sketch.js
@@ -0,0 +1,22 @@
+
+function setup() {
+    createCanvas(400, 400);
+}
+
+
+
+function draw() {
+    background("lightgrey");
+    fill("black");
+    textSize(64);
+    textAlign (CENTER, CENTER);
+    
+    let maximo = width;
+    let minimo = 0;
+    let palavra = "amo a bibia!!!";
+    let quantidade = map(mouseX, 0, width, 1, palavra.length);
+    let parcial = palavra.substring(0, quantidade);
+    text(parcial, 200, 200);
+    
+
+}
